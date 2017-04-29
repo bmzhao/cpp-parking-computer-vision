c032ad5d3d8aaac17ae4c8111979445d3d1df9d0
Brian Zhao <bzspartan@gmail.com>
2017-04-29 16:50:43 -0700

    added convenience functions

--- src/demo.py
+++ src/demo.py
@@ -15,7 +15,11 @@ RES_DIR = os.path.normpath(os.path.join(ROOT_DIR, 'res'))
 if __name__ == '__main__':
 
     cap = cv2.VideoCapture(os.path.join(RES_DIR, 'output.MP4'))
+    # cap = cv2.VideoCapture(os.path.join(RES_DIR, 'CarsDrivingUnderBridge.mp4'))
+
     bg_subtract = createBackgroundSubtractorMOG()
+    # bg_subtract = createBackgroundSubtractorGMG()
+    # bg_subtract = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 
     while True:
         ret, frame = cap.read()
@@ -27,17 +31,20 @@ if __name__ == '__main__':
         subtracted = bg_subtract.apply(preprocessed, learningRate=0.001)
         de_noised = util.remove_noise(subtracted)
 
-        contours = util.get_contours(de_noised,min_area=5000)
+        contours = util.get_contours(de_noised,min_area=2000)
 
         de_noised = util.gray_to_color(de_noised)
 
         cv2.drawContours(de_noised, contours, -1, (0, 255, 0), 3)
 
-        cv2.imshow('frame', de_noised)
+        # cv2.imshow('frame', de_noised)
 
+        # cv2.imshow('subtracted', subtracted)
 
-        # frame = bg_subtract.apply(frame, learningRate=0.0001)
+        cv2.imshow('frame', util.combine_frames(preprocessed, de_noised, subtracted))
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print('User pressed quit')
             break
 
     cap.release()
--- src/util.py
+++ src/util.py
@@ -19,23 +19,24 @@ def auto_canny(image, sigma=0.33):
     return edged
 
 
-def shrink(frame):
+def shrink(frame, ratio=2.0):
     height, width, depth = frame.shape
-    return cv2.resize(frame, (width // 2, height // 2), interpolation=cv2.INTER_AREA)
+    return cv2.resize(frame, (int(width / ratio), int(height / ratio)), interpolation=cv2.INTER_AREA)
 
 
 def preprocess(frame):
     '''
-    blurs and grayscales input frame into output frame
+    shrinks, blurs, and grayscales input frame into output frame
     :param frame: frame of video
     :return: output
     '''
-    if frame.shape[0] >= 720:
+    if frame.shape[0] > 720:
         frame = shrink(frame)
 
     result = cv2.GaussianBlur(frame, (5, 5), 2)
     result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     return result
+
 
 def gray_to_color(frame):
     return cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
@@ -45,12 +46,35 @@ def remove_noise(frame):
     # erosion = cv2.erode(frame, kernel, iterations=1)
     # result = cv2.dilate(frame, kernel, iterations=3)
     # result = cv2.dilate(frame, small_kernel, iterations=1)
-    return cv2.morphologyEx(frame, cv2.MORPH_OPEN, small_kernel)
+    result = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=3)
+    return result
 
 
-def get_contours(frame, min_area=minimum_area):
+def get_contours(frame, min_area=minimum_area, debug=False):
     image, contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if debug:
+        for contour in contours:
+            print(cv2.contourArea(contour))
+
     potential_cars = list(filter(lambda contour: cv2.contourArea(contour) > min_area, contours))
-    # for car in potential_cars:
-    #     print(cv2.contourArea(car))
+    potential_cars = list(map(cv2.convexHull, potential_cars))
+
     return potential_cars
+
+
+def combine_frames(*frames):
+    '''
+    stack frames vertically together 
+    useful for debugging various filters' effects 
+    :param frames: all the frames to combine, assuming all have same rows and heights dimension
+    :return: single numpy array of all frames stacked
+    '''
+    height = sum(map(lambda x: x.shape[0],frames))
+    ratio = height/900 #I chose 900 becuase my monitor has height of 900 pixels
+    def toColor(frame):
+        if len(frame.shape) != 3:
+            return shrink(cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR), ratio=ratio)
+        else:
+            return shrink(frame, ratio=ratio)
+
+    return np.vstack(map(toColor, frames))
